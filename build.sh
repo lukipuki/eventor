3249606d496d71ec4f48d727c7636d21ba136632
Lukáš Poláček <lukas@ksp.sk>
2017-12-31 11:47:16 +0100

    Switch to mcs from dmcs

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-dmcs -r:NHibernate.dll,Iesi.Collections.dll,FluentNHibernate.dll,MySql.Data.dll,System.Configuration.dll,System.Xml.Linq.dll Synchronization.cs Config.cs Model.cs ModelMapping.cs Save.cs Util.cs
-dmcs -t:library -r:NHibernate.dll,Iesi.Collections.dll,FluentNHibernate.dll,MySql.Data.dll,System.Configuration.dll,System.Xml.Linq.dll Synchronization.cs Config.cs Model.cs ModelMapping.cs Save.cs Util.cs
+mcs -r:NHibernate.dll,Iesi.Collections.dll,FluentNHibernate.dll,MySql.Data.dll,System.Configuration.dll,System.Xml.Linq.dll Synchronization.cs Config.cs Model.cs ModelMapping.cs Save.cs Util.cs
+mcs -t:library -r:NHibernate.dll,Iesi.Collections.dll,FluentNHibernate.dll,MySql.Data.dll,System.Configuration.dll,System.Xml.Linq.dll Synchronization.cs Config.cs Model.cs ModelMapping.cs Save.cs Util.cs
